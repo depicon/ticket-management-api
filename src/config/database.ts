2e256428e2e16873f43ee158d25b102b76908fe7
depicon <depicon@gmail.com>
2026-05-14 00:53:41 +0300

    feat(config): Implement dynamic CORS and robust environment loading
    Configure CORS to dynamically accept origins from the ALLOWED_ORIGINS environment variable. Update environment variable loading to be more robust based on NODE_ENV. Refine database configuration to strictly use the DATABASE_HOST environment variable. Adjust Dockerfile, build scripts, and .dockerignore for improved deployment and configuration.

--- src/config/database.ts
+++ src/config/database.ts
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const pool = new Pool({
-  host: process.env.DATABASE_HOST || 'localhost',
+  host: process.env.DATABASE_HOST,
   port: parseInt(process.env.DATABASE_PORT || '5432'),
   user: process.env.DATABASE_USER || 'postgres',
   password: process.env.DATABASE_PASSWORD || 'cl_pass',
--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,37 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import path from 'node:path';
 import ticketRoutes from './routes/ticketRoutes';
 import { errorHandler, notFoundHandler } from './middlewares/errorHandler';
 import { CorsOptions } from 'cors';
 import cors from 'cors'
 
-dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
-const allowedOrigins = ['http://localhost:5173'];
-const options: CorsOptions = {
-  origin: allowedOrigins
-}
+const env = process.env.NODE_ENV || 'development'
+// const port = process.env.PORT || 3000;
+// const allowedOrigins = ['http://localhost:5173'];
+// const options: CorsOptions = {
+//   origin: allowedOrigins
+// }
+
+dotenv.config({
+});
+
+const port = process.env.PORT || 3000
+const origins = process.env.ALLOWED_ORIGINS ? process.env.ALLOWED_ORIGINS.split(',') : [];
 
 // Middleware
-app.use(cors(options));
+app.use(cors({
+  origin: function (origin, callback) {
+    // Autorise les requêtes sans origine (comme Postman) ou les origines dans la liste
+    if (!origin || origins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Non autorisé par CORS'));
+    }
+  }
+}));
 app.use(express.json());
 
 // Routes
@@ -32,7 +48,7 @@ app.use(errorHandler);
 
 // Start server
 app.listen(port, () => {
-  console.log(`🚀 Server running on http://localhost:${port}`);
+  console.log(`🚀 Server running on http://localhost:${port} or ${process.env.PORT} and path: ${path.resolve(`.env.${env}`)}`);
 });
 
 export default app;
